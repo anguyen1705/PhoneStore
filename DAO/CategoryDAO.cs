d79056756993580fecebcc627c226495a72ce0c1
anguyen206 <anguyen206@dxc.com>
2021-08-14 19:02:47 +0700

    Admin manage product

---
+++ DAO/CategoryDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhoneStore.Models.db;
+using PhoneStore.Models.Entities;
+
+namespace PhoneStore.DAO
+{
+    public class CategoryDAO
+    {
+        PhoneStoreEntities db = new PhoneStoreEntities();
+        public List<Category> GetCategories()
+        {
+            String sql = "SELECT c.id,c.name,c.code,c.description,c.activeFlag,c.createDate,c.updateDate,c.parentId FROM category p join category c on c.parentId= p.id";
+
+            List<category> categories = db.categories.SqlQuery(sql).ToList<category>();
+            List<Category> cates = new List<Category>();
+            foreach (category c in categories)
+            {
+                cates.Add(new Category(c));
+            }
+
+            return cates;
+        }
+    }
+}
